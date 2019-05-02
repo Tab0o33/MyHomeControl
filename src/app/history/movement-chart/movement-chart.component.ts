04ec4695653b2feb7ef56e49c801a919b5f2a707
Tab0o33 <w.moinet@orange.fr>
2019-05-02 21:43:58 +0200

    Update History. Ajout des 2 autres courbes de mouvement

--- src/app/history/movement-chart/movement-chart.component.ts
+++ src/app/history/movement-chart/movement-chart.component.ts
@@ -2,61 +2,190 @@ import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
 
 @Component({
-  selector: 'app-movement-chart',
-  templateUrl: './movement-chart.component.html',
-  styleUrls: ['./movement-chart.component.scss']
+    selector: 'app-movement-chart',
+    templateUrl: './movement-chart.component.html',
+    styleUrls: ['./movement-chart.component.scss']
 })
 export class MovementChartComponent implements OnInit {
 
-  movementStackedChart = [];
+    movementStackedWeekChart = [];
+    movementPolarAreaChart = [];
+    movementStackedHoursChart = [];
 
-  constructor() { }
+    steppedActive: boolean = true;
+    stackedActive: boolean = false;
+    pieActive: boolean = false;
 
-  ngOnInit() {
+    constructor() { }
 
-    let movementPercentage = [12, 20, 18, 59, 31, 60, 76];
-    let luminosityDates = ['23/12/2012, 03:00:00', '24/12/2012, 03:00:00', 
-                           '25/12/2012, 03:00:00', '26/12/2012, 03:00:00',
-                           '27/12/2012, 03:00:00', '28/12/2012, 03:00:00',
-                           '29/12/2012, 03:00:00'];
+    ngOnInit() {
 
-    this.movementStackedChart = new Chart('movementStackedCanvas', {
-        type: 'bar',
-        data: {
-            labels: luminosityDates,
-            xAxisID: "temps",
-            yAxisID: "luminosité",
-            datasets: [
-              {
-                  data: movementPercentage,
-                  label: "Mouvement par jour (%)",
-                  backgroundColor: 'rgba(0, 200, 0, 0.7)',
-                  borderColor: 'rgba(0, 200, 0, 1)',
-                  borderWidth: 1
-              }
-            ]
-        },
-        options: {
-            legend: {
-                display: true
-            },
-            scales: {
-                xAxes: [{
-                    display: true,
-                    stacked: true
-                }],
-                yAxes: [{
-                    display: true,
-                    //stacked: true,
-                    ticks: {
-                        beginAtZero: true,
-                        max: 100,
+        let movementWeekPercentage = [12, 20, 18, 59, 31, 60, 76];
+        let movementPolarArea = [50, 80, 10, 20];
+        let movementHoursPercentage = [
+            12, 20, 18, 59, 31, 60, 76,
+            12, 20, 18, 59, 31, 60, 76,
+            12, 20, 18, 59, 31, 60, 76
+        ];
+
+        let movementWeekDates = [
+            '23/12/2012, 03:00:00',
+            '24/12/2012, 03:00:00',
+            '25/12/2012, 03:00:00',
+            '26/12/2012, 03:00:00',
+            '27/12/2012, 03:00:00',
+            '28/12/2012, 03:00:00',
+            '29/12/2012, 03:00:00'
+        ];
+
+        let movementHoursDates = [
+            '23/12/2012, 03:00:00',
+            '24/12/2012, 03:00:00',
+            '25/12/2012, 03:00:00',
+            '26/12/2012, 03:00:00',
+            '27/12/2012, 03:00:00',
+            '28/12/2012, 03:00:00',
+            '29/12/2012, 03:00:00',
+            '23/12/2012, 03:00:00',
+            '24/12/2012, 03:00:00',
+            '25/12/2012, 03:00:00',
+            '26/12/2012, 03:00:00',
+            '27/12/2012, 03:00:00',
+            '28/12/2012, 03:00:00',
+            '29/12/2012, 03:00:00',
+            '23/12/2012, 03:00:00',
+            '24/12/2012, 03:00:00',
+            '25/12/2012, 03:00:00',
+            '26/12/2012, 03:00:00',
+            '27/12/2012, 03:00:00',
+            '28/12/2012, 03:00:00',
+            '29/12/2012, 03:00:00'
+        ];
+
+        this.movementStackedWeekChart = new Chart('movementStackedWeekCanvas', {
+            type: 'bar',
+            data: {
+                labels: movementWeekDates,
+                xAxisID: "temps",
+                yAxisID: "mouvement",
+                datasets: [
+                    {
+                        data: movementWeekPercentage,
+                        label: "Mouvement de la semaine par jour (%)",
+                        backgroundColor: 'rgba(0, 200, 0, 0.7)',
+                        borderColor: 'rgba(0, 200, 0, 1)',
+                        borderWidth: 1
                     }
-                }],
+                ]
+            },
+            options: {
+                legend: {
+                    display: true
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        stacked: true
+                    }],
+                    yAxes: [{
+                        display: true,
+                        //stacked: true,
+                        ticks: {
+                            beginAtZero: true,
+                            max: 100,
+                        }
+                    }],
+                }
             }
-        }
-    });
+        });
 
-  }
+        this.movementPolarAreaChart = new Chart('movementPolarAreaCanvas', {
+            type: 'polarArea',
+            data: {
+                labels: ['0h-6h', '6h-12h', '12h-18h', '18h-0h'],
+                xAxisID: "temps",
+                yAxisID: "mouvement",
+                datasets: [
+                    {
+                        data: movementPolarArea,
+                        label: "Mouvement par jour (%)",
+                        backgroundColor: ['rgba(0, 200, 0, 0.7)',
+                            'rgba(200, 200, 0, 0.7)',
+                            'rgba(0, 200, 200, 0.7)',
+                            'rgba(200, 0, 200, 0.7)'],
+                        borderColor: 'rgba(0, 0, 0, 0.2)',
+                        borderWidth: 1
+                    }
+                ]
+            },
+            options: {
+                legend: {
+                    display: true
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        stacked: true
+                    }],
+                    yAxes: [{
+                        display: true,
+                        //stacked: true,
+                        ticks: {
+                            beginAtZero: true,
+                            max: 100,
+                        }
+                    }],
+                }
+            }
+        });
+
+        this.movementStackedHoursChart = new Chart('movementStackedHoursCanvas', {
+            type: 'bar',
+            data: {
+                labels: movementHoursDates,
+                xAxisID: "temps",
+                yAxisID: "mouvement",
+                datasets: [
+                    {
+                        data: movementHoursPercentage,
+                        label: "Mouvement par tranche de x min sur 4h (%)",
+                        backgroundColor: 'rgba(0, 200, 0, 0.7)',
+                        borderColor: 'rgba(0, 200, 0, 1)',
+                        borderWidth: 1
+                    }
+                ]
+            },
+            options: {
+                legend: {
+                    display: true
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        stacked: true
+                    }],
+                    yAxes: [{
+                        display: true,
+                        //stacked: true,
+                        ticks: {
+                            beginAtZero: true,
+                            max: 100,
+                        }
+                    }],
+                }
+            }
+        });
+
+    }
+
+    switchStepped() {
+        this.steppedActive = !this.steppedActive;
+    }
+    switchStacked() {
+        this.stackedActive = !this.stackedActive;
+    }
+    switchPie() {
+        this.pieActive = !this.pieActive;
+    }
 
 }
